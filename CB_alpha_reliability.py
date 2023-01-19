89c16bbff3a660f0136588d246dd355b9e1e8017
[A[A <>
2023-01-19 17:52:21 +0530

    Reliabilty measure of STAI using cronsbach alpha

--- CB_alpha_reliability.py
+++ CB_alpha_reliability.py
@@ -5,6 +5,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 import openpyxl
+import colorama
+from colorama import Fore ,Back, Style
+from simple_colors import *
 
 
 
@@ -19,20 +22,20 @@ print("Cronbach's_alpha_score=",temp,"\n")'''
 
 def cronsbach_alpha(data,prefix):
     print(data,"\n")
-    print("Correlation \n \n",data.corr(),"\n")
+    print(Fore.YELLOW +"Correlation \n \n",data.corr(),"\n")
     sns.heatmap(data.corr(), cmap='RdBu',vmin=-1, vmax=1, linewidth=0.5, annot=True,annot_kws={'fontsize':11, 'fontweight':'bold'},square=True)
     plt.title(prefix+"_correlation")
     plt.show()
     df_trans=data.transpose()
     # Drop the column Qusetionarie => value is not neumeric
     df_trans=df_trans.drop(labels='Qusetionarie',  axis=0)
-    print(df_trans)
+    print(Fore.CYAN+"After Transpose =\n\n",df_trans,"\n")
     df_dict = df_trans.to_dict()
-    print("\n\n",df_dict,"\n")
+    print(Fore.YELLOW+"Dictonary_Form=\n\n",df_dict,"\n")
     df_2= pd.DataFrame(df_dict)
     temp_2=pg.cronbach_alpha(data=df_2,ci=.95)
     print("\n")
-    print( prefix+"_Cronbach's_alpha_score=",temp_2,"\n")
+    print(Fore.GREEN+ prefix+"_Cronbach's_alpha__reliablity_score=",temp_2,"\n")
     pass
 
 def main():
